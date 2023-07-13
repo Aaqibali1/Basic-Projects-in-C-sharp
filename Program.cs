a7b151ff6656d4561d32f7ce79d83d350d2ffa67
Aaqib Ali <75785706+Aaqibali1@users.noreply.github.com>
2023-07-13 20:41:40 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter your another Number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2);
+        Console.WriteLine(num1 - num2);
+        Console.WriteLine(num1 / num2);
+        Console.WriteLine(num1 % num2);
+
+    }
+}
